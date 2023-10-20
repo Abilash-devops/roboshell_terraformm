dd407c12f41e15ceeb555c5cf093f59d9bfcb3d0
Abilash-devops <abilashdevopsaws@gmail.com>
2023-10-20 15:47:19 +0530

    commit

--- payment.sh
+++ payment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 D=$(date +%F:%H:%M:%S)
 SCRIPT_NAME=$0
-LOG_PATH=/home/centos/roboshell/logs
+LOG_PATH=/home/centos/roboshell_terraformm/logs
 LOGFILE=$LOG_PATH/$0-$D-log
 u=$(id -u)
 R="\e[31m"
@@ -47,7 +47,7 @@ pip3.6 install -r requirements.txt &>> $LOGFILE
 
 validate $? "depencencies install"
 
-cp -rp /home/centos/roboshell/payment.service /etc/systemd/system/payment.service &>> $LOGFILE
+cp -rp /home/centos/roboshell_terraformm/payment.service /etc/systemd/system/payment.service &>> $LOGFILE
 
 validate $? "payment service file creation"
 
